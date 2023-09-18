e4432ee51fe7cd7ec756df939ea460941a98fcb9
FelipeHardmann <fashardmann@gmail.com>
2023-09-18 15:39:42 -0300

    :sparkles: feat: method get

--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Skills
 
-# Create your views here.
+
+class SkillList(View):
+    def get(self, request):
+        skills = Skills.objects.all()
+        data = [skill.to_json() for skill in skills]
+        return JsonResponse(data, safe=False)

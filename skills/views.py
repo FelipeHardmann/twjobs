446e1459709c05e9181c88b443ad2d1fe2cab77d
FelipeHardmann <fashardmann@gmail.com>
2023-09-18 23:47:28 -0300

    :sparkles: feat: methods rest

--- skills/views.py
+++ skills/views.py
@@ -1,10 +1,26 @@
-from django.http import JsonResponse
-from django.views import View
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Skills
+from .serializers import SkillSerializer
+from rest_framework import status
 
 
-class SkillList(View):
+class SkillList(APIView):
     def get(self, request):
         skills = Skills.objects.all()
-        data = [skill.to_json() for skill in skills]
-        return JsonResponse(data, safe=False)
+        serializer = SkillSerializer(skills, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = SkillSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SkillDetail(APIView):
+    def get(self, request, pk):
+        skill = get_object_or_404(Skills, pk=pk)
+        serializer = SkillSerializer(skill)
+        return Response(serializer.data)

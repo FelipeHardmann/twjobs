b771e80d33582bac0841df8c9c046a1c9965b611
FelipeHardmann <fashardmann@gmail.com>
2023-09-19 15:49:50 -0300

    :recycle: refactore: http functions

--- skills/views.py
+++ skills/views.py
@@ -14,13 +14,25 @@ class SkillList(APIView):
     
     def post(self, request):
         serializer = SkillSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+
 class SkillDetail(APIView):
     def get(self, request, pk):
         skill = get_object_or_404(Skills, pk=pk)
         serializer = SkillSerializer(skill)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        skill = get_object_or_404(Skills, pk=pk)
+        serializer = SkillSerializer(skill, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        skill = get_object_or_404(Skills, pk=pk)
+        skill.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

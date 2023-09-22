1d02738de89c36f71f87dc7d51282b52483836a2
FelipeHardmann <fashardmann@gmail.com>
2023-09-21 23:46:26 -0300

    :sparkles: feat: validates

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -6,3 +6,31 @@ class JobsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Jobs
         exclude = ('is_active',)
+
+    def validate_title(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError(
+                'Deve ter pelo menos 10 caracteres'
+            )
+        return value
+
+    def validate_salary(self, value):
+        if value < 1000:
+            raise serializers.ValidationError(
+                'O campo deve ser maior do que 1000'
+            )
+        return value
+
+    def validate_description(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError(
+                'Deve ter pelo menos 10 caracteres'
+            )
+        return value
+
+    def validate_company(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                'Deve ter pelo menos 3 caracteres'
+            )
+        return value

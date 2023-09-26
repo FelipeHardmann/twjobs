08ca06fdfea07995d22df6738b19bb72bc7947d6
FelipeHardmann <fashardmann@gmail.com>
2023-09-25 23:28:53 -0300

    :wrench: refactore settings

--- setup/settings.py
+++ setup/settings.py
@@ -47,7 +47,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "jobs.apps.JobsConfig",
-    "skills.apps.SkillsConfig"
+    "skills.apps.SkillsConfig",
+    "core.apps.CoreConfig"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -138,6 +139,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10
+    "DEFAULT_PAGINATION_CLASS": "core.paginations.TwJobsPagination",
+    "PAGE_SIZE": 10,
 }
+
+PAGE_SIZE_QUERY_PARAM = "size",
+MAX_PAGE_SIZE = 100
